bd33cf79c84758dada4ddd90d311fc299402570e
Mátyás Őri <ori.matyas@gmail.com>
2023-07-06 10:31:30 +0200

    v2

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -4,15 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Menu();
+            var date = DateTime.Now;
+            string name = GetName();
+            
+            Menu(name);
 
-            void Menu()
+            string GetName()
             {
                 Console.WriteLine("Hello, \nPlease type your name:");
                 var name = Console.ReadLine();
                 Console.Clear();
-                Console.WriteLine($"Hello {name}. It's time to work on your math skills!\n");
-                Console.WriteLine(@"What do you want to practise? Please select from down below:
+                return name;
+            }
+            void Menu(string name)
+            {
+                Console.WriteLine($"Hello {name}. Today it's {date}. It's time to work on your math skills!\n");
+                Console.WriteLine(@"What do you want to practice? Please select from down below:
             A - Addition
             S - Substraction
             M - Multiplication
@@ -44,21 +51,92 @@
                         break;
                 }
             }
-
             void AdditionGame(string message)
             {
                 Console.Clear();
                 Console.WriteLine(message);
+
+                var random = new Random();
+                int firstNumber;
+                int secondNumber;
+                int score = 0;
+                for (int i = 1 ; i < 5; i++)
+                {
+                    firstNumber = random.Next(0, 10);
+                    secondNumber = random.Next(0, 10);
+
+                    Console.WriteLine($"{firstNumber} + {secondNumber} = ?");
+                    var result = Console.ReadLine();
+                    
+                    if (int.Parse(result) == firstNumber + secondNumber)
+                    {
+                        Console.WriteLine("Correct answer!");
+                        score++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Wrong, the correct answer is: {firstNumber + secondNumber}");
+                    }
+                }
+                Console.WriteLine($"Final score: {score}");
             }
             void SubstractionGame(string message)
             {
                 Console.Clear();
                 Console.WriteLine(message);
+
+                var random = new Random();
+                int firstNumber;
+                int secondNumber;
+                int score = 0;
+                for (int i = 1; i < 5; i++)
+                {
+                    firstNumber = random.Next(0, 10);
+                    secondNumber = random.Next(0, 10);
+
+                    Console.WriteLine($"{firstNumber} - {secondNumber} = ?");
+                    var result = Console.ReadLine();
+
+                    if (int.Parse(result) == firstNumber - secondNumber)
+                    {
+                        Console.WriteLine("Correct answer!");
+                        score++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Wrong, the correct answer is: {firstNumber - secondNumber}");
+                    }
+                }
+                Console.WriteLine($"Final score: {score}");
             }
             void MultiplicationGame(string message)
             {
                 Console.Clear();
                 Console.WriteLine(message);
+
+                var random = new Random();
+                int firstNumber;
+                int secondNumber;
+                int score = 0;
+                for (int i = 1; i < 5; i++)
+                {
+                    firstNumber = random.Next(0, 10);
+                    secondNumber = random.Next(0, 10);
+
+                    Console.WriteLine($"{firstNumber} * {secondNumber} = ?");
+                    var result = Console.ReadLine();
+
+                    if (int.Parse(result) == firstNumber * secondNumber)
+                    {
+                        Console.WriteLine("Correct answer!");
+                        score++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Wrong, the correct answer is: {firstNumber * secondNumber}");
+                    }
+                }
+                Console.WriteLine($"Final score: {score}");
             }
             void DivisionGame(string message)
             {
